26dba69531ea504875110f9669c46eedb5586042
Aymeric Bouchet <aymeric.bouchet@viacesi.fr>
2022-03-07 14:52:32 +0100

    ✨Add card flip

---
+++ src/main/memory.js
@@ -0,0 +1,20 @@
+import { createGrid } from "./grid.js";
+import delay from "./delay.js";
+
+document.addEventListener("DOMContentLoaded",()=>{
+    console.log("hello!");
+    const grid = createGrid(null)
+    for (const [i,row] of grid.entries()) {
+        for (const [j,card] of row.entries()) {
+            const cell = document.querySelector("#row_"+i+"-col_"+j)
+            console.log(i,j,cell);
+            cell.addEventListener("click",()=>{
+                cell.src = "images/image1.jpeg"
+                delay(1000).then(()=>{
+                    cell.src = "images/dos.jpeg"
+                })
+            })
+        }
+    }
+    console.table("grid",grid);
+})
---
+++ src/test/testCardSwitch.js
@@ -0,0 +1,18 @@
+// import { describe } from "mocha";
+import { createGrid } from "../main/grid.js";
+import chai from 'chai'
+const expect = chai.expect
+
+describe("a memory grid",()=>{
+
+    it("must contain cards",()=>{
+
+        const grid = createGrid(null)
+        expect(grid).to.not.be.null
+        const card = grid[4][2]
+        expect(card).to.not.be.null
+        expect(card.visible).to.be.false
+        expect(card.face).to.not.be.empty
+        expect(card.face).to.eql('back')
+    })
+})
